af35e3167e66e0b5a50333dfdaeac6694b37eebc
Jerin Jacob <jerin.jacob@caviumnetworks.com>
2015-02-06 23:08:16 +0530

    usefull commands

--- commands.sh
+++ commands.sh
@@ -5,3 +5,5 @@ ctags $(find ../../components/ ../../examples/ ../../executive -name \*[.chS])
 ctags $(find . -name \*[.chS])
 git diff > /tmp/git_diff && ./scripts/checkpatch.pl /tmp/git_diff
 rysnc -aH src dest
+
+./configure --host=mipsisa64-octeon-elf --with-platform=octeon --with-soc-model=cn68xx_pass2_2 --enable-bare-metal --enable-shared=no --enable-cunit --with-cunit-path=/export/cunit-code/prefix
