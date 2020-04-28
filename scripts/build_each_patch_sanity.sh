51c8e469f22999f20c85c1b7f21f5579af79324f
Jerin Jacob <jerinj@marvell.com>
2020-04-28 19:29:50 +0530

    update configs
    Signed-off-by: Jerin Jacob <jerinj@marvell.com>

--- scripts/build_each_patch_sanity.sh
+++ scripts/build_each_patch_sanity.sh
@@ -3,7 +3,6 @@
 
 
 files=$1/*
-cpus=`getconf _NPROCESSORS_ONLN`
 
 changeset=`git log --oneline | head -n 1 | cut -f 1 -d " "`
 
@@ -12,45 +11,23 @@ if [ "$1" == "" ]; then
 	exit
 fi
 
-echo "gcc test"
+rm -rf build && meson -Dexamples=all build  1> /tmp/build.log 2> /tmp/build.log
+if [ $? -ne 0 ]; then
+	echo "config failed"
+	exit
+fi
+
 for f in $files
 do
-	#echo "$f"
-	rm -rf build
-	git clean -xdf 2>/dev/null 1>/dev/null
 	git am -3 $f
 	if [ $? -ne 0 ]; then
 		git reset --hard $changeset
 		echo "git am failed $f"
 		exit
 	fi
-	make config T=x86_64-native-linux-gcc 1> /tmp/build.log
+	ninja -C build 1> /tmp/build.log 2> /tmp/build.log
 	if [ $? -ne 0 ]; then
-		echo "make config failed $f"
-		git reset --hard $changeset
-		exit
-	fi
-
-	CC="ccache gcc"  make -j $cpus 1> /tmp/build.log
-	if [ $? -ne 0 ]; then
-		echo "make failed $f"
-		git reset --hard $changeset
-		exit
-	fi
-
-	CC="ccache gcc"  make -j $cpus test-build 1> /tmp/build.log
-	if [ $? -ne 0 ]; then
-		echo "make test-build failed $f"
-		git reset --hard $changeset
-		exit
-	fi
-
-	export RTE_SDK=`pwd`
-	export RTE_TARGET=build
-	CC="ccache gcc"  make -j $cpus -C examples 1> /tmp/build.log
-	if [ $? -ne 0 ]; then
-		echo "make example failed $f"
-		git reset --hard $changeset
+		echo "build failed $f"
 		exit
 	fi
 done
