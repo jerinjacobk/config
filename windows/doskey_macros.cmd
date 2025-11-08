e5abf7ed257762044fa9c98309145cc98e29349c
Jerin Jacob <jerinjacobk@gmail.com>
2025-11-08 18:55:05 +0530

    windows: emulate linux env on windows
    Create shortcut with cmd.exe /K startup_env.bat
    
    Signed-off-by: Jerin Jacob <jerinjacobk@gmail.com>

---
+++ windows/doskey_macros.cmd
@@ -0,0 +1,23 @@
+clink="C:\Program Files (x86)\clink\clink_x64.exe" $*
+vim="C:\Program Files\Vim\vim91\vim.exe" $*
+history="C:\Program Files (x86)\clink\clink_x64.exe" history $*
+grep=ugrep $*
+ls=coreutils ls --color=auto $*
+rm=coreutils rm $*
+df=coreutils df $*
+v="C:\Program Files\Vim\vim91\vim.exe" $*
+diff="C:\Program Files\Vim\vim91\diff.exe" $*
+q=git clean -dxf $*
+htop="C:\Users\jerin\AppData\Local\Microsoft\WinGet\Packages\gsass1.NTop_Microsoft.Winget.Source_8wekyb3d8bbwe\ntop.exe" $*
+cp=coreutils cp $*
+mv=coreutils mv $*
+mkdir=coreutils mkdir $*
+r=git pull --rebase $*
+pip=uv pip $*
+locate="C:\GnuWin32\bin\locate.exe" $*
+find="C:\GnuWin32\bin\find.exe" $*
+nbconvert=jupyter-nbconvert.exe --ClearOutputPreprocessor.enabled=True --inplace $*
+xargs="C:\GnuWin32\bin\xargs.exe" $*
+ctags="C:\Users\jerin\AppData\Local\Microsoft\WinGet\Packages\UniversalCtags.Ctags_Microsoft.Winget.Source_8wekyb3d8bbwe\ctags.exe" $*
+clear=cls
+r=git pull --rebase
---
+++ windows/startup_env.bat
@@ -0,0 +1,12 @@
+@echo off
+REM === Load your DOSKEY macros ===
+doskey /macrofile="C:\Users\jerin\doskey_macros.cmd"
+
+REM === Change to working directory ===
+cd /d "C:\Users\jerin\jtrade"
+
+REM === Activate your Python virtual environment ===
+call ".venv\Scripts\activate"
+
+REM === Optional: show where we are ===
+echo Working directory: %CD%
